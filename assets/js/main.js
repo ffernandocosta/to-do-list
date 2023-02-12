ad05ca9ccb074271a92915ac7101817d2ce59ccb
Fernando Costa <ferndcosta353@gmail.com>
2023-02-12 11:27:45 -0300

    Implementing Todo Task Input and Submit
    added an eventListener of click to add what the user writes to the tasks array

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,12 @@
 let tasks = [];
 
+
 const taskInputEl = document.getElementById("addtask-input");
 const submitTaskEl = document.getElementById("addtask-btn");
+const todoListUl = document.querySelector(".todo-tasks");
+
+
+submitTaskEl.addEventListener("click", (e) => {
+    e.preventDefault();
+    tasks.push(taskInputEl.value)
+})
