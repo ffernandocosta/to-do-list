ef717a409be77d5213e9be575e28516e2e1c03cd
Fernando Costa <ferndcosta353@gmail.com>
2023-02-13 14:22:35 -0300

    adding renderTasks function
    The function first clears the existing HTML content of the 'todoListUl' element and then loops through each task in the 'tasks' array creating a new 'li' element everytime the user types and clicks on the 'addtask-btn'

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,6 +7,20 @@ const todoListUl = document.querySelector(".todo-tasks");
 
 
 submitTaskEl.addEventListener("click", (e) => {
+    
     e.preventDefault();
     tasks.push(taskInputEl.value)
-})
+    renderTasks()
+});
+
+function renderTasks() {
+
+    todoListUl.innerHTML = "";
+    
+    for (let i = 0; i < tasks.length; i++) {
+        const liTaskEl = document.createElement("li")
+        liTaskEl.className = "task"
+        liTaskEl.textContent = tasks[i]
+        todoListUl.append(liTaskEl)
+    }
+}
